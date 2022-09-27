72aa79c8bd4decf7117af407a21f5d9c58241e7f
Pablo <pfadarraga@gmail.com>
2022-09-27 15:15:44 +0200

    component home

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
-</div>
+<section class="section home">
+  <div class="home__container">
+    <h1>Psicóloga Tudela, Navarra</h1>
+    <h3>Begoña Lardiés, Psicóloga Sanitaria en Tudela y Online</h3>
+    <a href="">Pide cita</a>
+  </div>
+</section>
