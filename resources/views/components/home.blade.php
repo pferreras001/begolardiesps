7b008d3207b4f8b9fd11f4d30dfe59bfc9daee84
Pablo <pfadarraga@gmail.com>
2022-10-25 19:02:32 +0200

    fixes

--- resources/views/components/home.blade.php
+++ resources/views/components/home.blade.php
@@ -1,7 +1,7 @@
 <section class="section home">
   <div data-aos="fade-right" class="home__container container">
     <h1>Psicóloga Tudela, Navarra</h1>
-    <h3>Begoña Lardiés, Psicóloga Sanitaria en Tudela y Online</h3>
+    <h3>Begoña Lardiés, Psicóloga Infanto-Juvenil en Tudela y Online</h3>
     <a class="btn1" href="{{ route('contacto') }}">Pide cita</a>
   </div>
 </section>
