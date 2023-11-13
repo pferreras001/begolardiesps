a631aac1d6946f7b6d0c2af750d8703557d96a76
pferreras001 <71315280+pferreras001@users.noreply.github.com>
2023-11-13 15:26:35 +0100

    Fixes

--- resources/views/components/sobre_mi.blade.php
+++ resources/views/components/sobre_mi.blade.php
@@ -1,19 +1,23 @@
 <section class="section sobre_mi" id="sobre_mi">
-  <div data-aos="fade-right" class="sobre_mi__container container">
-    <h1>Sobre mi.</h1>
-    <p>
-      ¡Bienvenida a mi página web!<br><br>
+    <img class="sobre_mi__background" src="img/sobre_mi/background.jpg"/>
+    <div data-aos="fade-right" class="sobre_mi__container container">
+        <div class="sobre_mi__left">
+            <h1>Sobre mi.</h1>
+        </div>
+        <div class="sobre_mi__right">
+            <p>
+            ¡Bienvenida a mi página web!<br><br>
 
-      Mi nombre es Begoña Lardiés. Soy psicóloga especializada en el ámbito de la <span>infancia y la adolescencia</span>. Actualmente trabajo como <span>psicóloga infantil</span> en la consulta presencial de <span>Andrea Pérez Psicología</span>, donde ayudamos a niños y adolescentes a manejar sus conflictos y dificultades personales. También llevo a cabo sesiones de forma <span>online</span> para aquellas personas que no puedan acudir a consulta de manera presencial o se sientan más cómodas a través de esta modalidad.<br><br>
+            Mi nombre es Begoña Lardiés. Soy psicóloga especializada en el ámbito de la <span>infancia y la adolescencia</span>. Actualmente trabajo como <span>psicóloga infantil</span> en la consulta presencial de <span>Andrea Pérez Psicología</span>, donde ayudamos a niños y adolescentes a manejar sus conflictos y dificultades personales. También llevo a cabo sesiones de forma <span>online</span> para aquellas personas que no puedan acudir a consulta de manera presencial o se sientan más cómodas a través de esta modalidad.<br><br>
 
-      Desde que tengo uso de razón el mundo de la <span>Psicología</span> me ha apasionado. Cuando era pequeña, sin ni siquiera saber lo que significaba eso de ser psicóloga, pasaba horas y horas viendo películas relacionadas con este mundillo y soñaba con ayudar a otros <span>niños</span> a “encontrarse un poquito mejor”. A día de hoy comprendo que he escogido el <span>camino</span> correcto y todo va cobrando <span>sentido</span>.<br><br>
+            Desde que tengo uso de razón el mundo de la <span>Psicología</span> me ha apasionado. Cuando era pequeña, sin ni siquiera saber lo que significaba eso de ser psicóloga, pasaba horas y horas viendo películas relacionadas con este mundillo y soñaba con ayudar a otros <span>niños</span> a “encontrarse un poquito mejor”. A día de hoy comprendo que he escogido el <span>camino</span> correcto y todo va cobrando <span>sentido</span>.<br><br>
 
-      <img src="img/sobre_mi/img_01.jpg"/>
+            <img src="img/sobre_mi/img_01.jpg"/>
 
-      En cuanto a mi metodología de trabajo, la orientación con la que más cómoda me he sentido hasta el momento es la <span>cognitivo-conductual</span> y de <span>tercera generación</span>. Parte de nuestro trabajo consiste en intervenir directamente con el menor, facilitando estrategias y herramientas que le ayuden a manejar las dificultades que atraviesa. Sin embargo, considero que es primordial atender a los distintos <span>entornos</span> en los que el niño o adolescente se desenvuelve para que pueda sentirse acompañado a través de una mirada comprensiva, de aceptación y respeto.<br><br>
+            En cuanto a mi metodología de trabajo, la orientación con la que más cómoda me he sentido hasta el momento es la <span>cognitivo-conductual</span> y de <span>tercera generación</span>. Parte de nuestro trabajo consiste en intervenir directamente con el menor, facilitando estrategias y herramientas que le ayuden a manejar las dificultades que atraviesa. Sin embargo, considero que es primordial atender a los distintos <span>entornos</span> en los que el niño o adolescente se desenvuelve para que pueda sentirse acompañado a través de una mirada comprensiva, de aceptación y respeto.<br><br>
 
-      Por ello, una parte fundamental de lo que hacemos consiste en acompañar y orientar a las <span>familias</span> de estos niños, pues muchas veces nos sentimos perdidos a la hora de acompañar a nuestros peques y no contamos con herramientas suficientes para comprenderlos y ayudarlos. Por ello, llevamos a cabo un proceso de orientación y <span>seguimiento</span> contínuo tanto con el entorno familiar del menor como en el <span>contexto escolar</span> en el que se desarrollan, facilitando pautas específicas con las que puedan ayudarlos de la mejor forma posible a manejar su malestar.<br><br>
-    </p>
-  </div>
-  <img src="/img/sobre_mi/background.jpg">
+            Por ello, una parte fundamental de lo que hacemos consiste en acompañar y orientar a las <span>familias</span> de estos niños, pues muchas veces nos sentimos perdidos a la hora de acompañar a nuestros peques y no contamos con herramientas suficientes para comprenderlos y ayudarlos. Por ello, llevamos a cabo un proceso de orientación y <span>seguimiento</span> contínuo tanto con el entorno familiar del menor como en el <span>contexto escolar</span> en el que se desarrollan, facilitando pautas específicas con las que puedan ayudarlos de la mejor forma posible a manejar su malestar.<br><br>
+            </p>
+        </div>
+    </div>
 </section>
